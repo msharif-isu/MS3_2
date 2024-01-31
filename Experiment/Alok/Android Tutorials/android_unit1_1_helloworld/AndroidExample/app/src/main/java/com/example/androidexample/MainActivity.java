dc2c4c6a4d0318fee1adfd93fc55da4581e46b7e
Alok Shrestha <aloks@iastate.edu>
2024-01-31 15:34:16 -0600

    Added existing work from repo

---
+++ Experiment/Alok/Android Tutorials/android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView messageText;   // define message textview variable
+    private Button funButton;       // define button variable
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);             // link to Main activity XML
+
+        /* initialize UI elements */
+        messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
+        messageText.setBackgroundColor(Color.rgb(100, 50, 50));
+    }
+}
