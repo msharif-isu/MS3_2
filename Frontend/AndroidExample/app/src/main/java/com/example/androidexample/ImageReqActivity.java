be6c515fd581c5ef2feed2d5079dbab1de024041
Alok Shrestha <aloks@iastate.edu>
2024-02-24 15:38:58 -0600

    Pulling in template Android Studio project from main

---
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/ImageReqActivity.java
@@ -0,0 +1,104 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImageReqActivity extends AppCompatActivity {
+
+    private Button btnImageReq;
+    private ImageView imageView;
+
+    public static final String URL_IMAGE = "https://47de868e-39ce-4710-a979-60d5051a5022.mock.pstmn.io/q1/image";
+
+    public static String imageURL = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_req);
+
+        btnImageReq = (Button) findViewById(R.id.btnImageReq);
+        imageView = (ImageView) findViewById(R.id.imgView);
+
+        btnImageReq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {getImageURL();}
+        });
+    }
+
+    private void getImageURL() {
+        StringRequest urlRequest = new StringRequest(
+            Request.Method.GET,
+            URL_IMAGE,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Handle the successful response here
+                        Log.d("Volley Response", response);
+                        System.out.println("Hi");
+                        imageURL = response;
+                        makeImageRequest();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle any errors that occur during the request
+                        Log.e("Volley Error", error.toString());
+                    }
+                }
+        );
+
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(urlRequest);
+    }
+
+    /**
+     * Making image request
+     * */
+    private void makeImageRequest() {
+
+        ImageRequest imageRequest = new ImageRequest(
+            imageURL,
+            new Response.Listener<Bitmap>() {
+                @Override
+                public void onResponse(Bitmap response) {
+                    // Display the image in the ImageView
+                    imageView.setImageBitmap(response);
+                }
+            },
+            0, // Width, set to 0 to get the original width
+            0, // Height, set to 0 to get the original height
+            ImageView.ScaleType.FIT_XY, // ScaleType
+            Bitmap.Config.RGB_565, // Bitmap config
+
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // Handle errors here
+                    Log.e("Volley Error", error.toString());
+                }
+            }
+        );
+
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(imageRequest);
+    }
+
+}
