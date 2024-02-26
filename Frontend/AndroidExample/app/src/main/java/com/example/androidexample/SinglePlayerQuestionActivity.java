5aad58e9823dc1231faf9b489d098931a1e1d85e
osamman <[200~glpat-2STCituse5BUyinw33Zy~>
2024-02-26 14:53:00 -0600

    Added decreasing timer that changes activity when time is up.

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/SinglePlayerQuestionActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/SinglePlayerQuestionActivity.java
@@ -1,16 +1,23 @@
 package com.example.androidexample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.Toast;
 
 public class SinglePlayerQuestionActivity extends AppCompatActivity {
 
     private TextView questionTextView, pointsTextView, timeLeftTextView;
     private EditText answerBoxEditText;
     private Button submitButton;
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +34,13 @@ public class SinglePlayerQuestionActivity extends AppCompatActivity {
         // Set initial text
         questionTextView.setText("Your question goes here");
         pointsTextView.setText("Points: 0");
-        timeLeftTextView.setText("Time Left: 10 minutes");
+
+        // Set initial time to 30 seconds
+        timeLeftInMillis = 30 * 1000; // 30 seconds
+        updateCountDownText();
+
+        // Start the countdown timer
+        startCountDown();
 
         // Set OnClickListener for the submit button
         submitButton.setOnClickListener(view -> {
@@ -35,5 +48,37 @@ public class SinglePlayerQuestionActivity extends AppCompatActivity {
             String answer = answerBoxEditText.getText().toString();
             // Do something with the answer
         });
+    }
+
+    private void startCountDown() {
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(getApplicationContext(), "Out of time.", Toast.LENGTH_LONG).show();
+                // Switch activities when timer finishes
+                startActivity(new Intent(SinglePlayerQuestionActivity.this, MainActivity.class));
+                finish();
+            }
+        }.start();
+    }
+
+    private void updateCountDownText() {
+        int seconds = (int) (timeLeftInMillis / 1000);
+        String timeLeftFormatted = String.format("%02d", seconds);
+        timeLeftTextView.setText("Time Left: " + timeLeftFormatted);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
     }
 }
