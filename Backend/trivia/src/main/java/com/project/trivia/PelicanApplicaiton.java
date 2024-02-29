82a12be70231bb0cd69dd3cac6ff528dd113944b
adamal22@iastate.edu <adamal22@iastate.edu>
2024-02-29 12:35:50 -0600

    minor changes

--- Backend/trivia/src/main/java/com/project/trivia/PelicanApplicaiton.java
+++ Backend/trivia/src/main/java/com/project/trivia/PelicanApplicaiton.java
@@ -1,14 +1,14 @@
-//package com.project.trivia;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class PelicanApplicaiton {
-//    public static void main(String[] args) {
-//        SpringApplication.run(com.project.trivia.PelicanApplicaiton.class, args);
-//    }
-//
+package com.project.trivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelicanApplicaiton {
+    public static void main(String[] args) {
+        SpringApplication.run(com.project.trivia.PelicanApplicaiton.class, args);
+    }
+
 
 //    @Bean
 //    CommandLineRunner initUser(PelicanQuestionRepository pepository) {
@@ -29,4 +29,4 @@
 //            pepository.save(pq6);
 //        };
 //    }
-//}
+}
--- Backend/trivia/src/main/java/com/project/trivia/QuestionApplication.java
+++ Backend/trivia/src/main/java/com/project/trivia/QuestionApplication.java
@@ -1,26 +1,26 @@
 package com.project.trivia;
-//
-//import com.project.trivia.Questions.Question;
-//import com.project.trivia.Questions.QuestionRepository;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
-//
-//@SpringBootApplication
-//public class QuestionApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(com.project.trivia.QuestionApplication.class, args);
+
+import com.project.trivia.Questions.Question;
+import com.project.trivia.Questions.QuestionRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class QuestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.project.trivia.QuestionApplication.class, args);
+    }
+
+//    @Bean
+//    CommandLineRunner initUser(QuestionRepository questionRepository) {
+//        return args -> {
+//            Question q1 = new Question("Who is the coolest?", "Raphael", "not a question");
+//            Question q2 = new Question("Who is the 2nd coolest?", "Also Raphael", "not a question");
+//            questionRepository.save(q1);
+//            questionRepository.save(q2);
+//        };
 //    }
-//
-////    @Bean
-////    CommandLineRunner initUser(QuestionRepository questionRepository) {
-////        return args -> {
-////            Question q1 = new Question("Who is the coolest?", "Raphael", "not a question");
-////            Question q2 = new Question("Who is the 2nd coolest?", "Also Raphael", "not a question");
-////            questionRepository.save(q1);
-////            questionRepository.save(q2);
-////        };
-////    }
-//}
+}
