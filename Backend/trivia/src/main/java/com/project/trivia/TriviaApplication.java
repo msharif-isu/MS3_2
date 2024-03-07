1472044eb6fa1b2ea197c3b4d84479f3ad3725fb
rbruce01@iastate.edu <glpat-EKjJtPzKhL46BZ7nT7dw>
2024-03-06 22:18:22 -0600

    added all files

--- Backend/trivia/src/main/java/com/project/trivia/TriviaApplication.java
+++ Backend/trivia/src/main/java/com/project/trivia/TriviaApplication.java
@@ -2,12 +2,14 @@ package com.project.trivia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TriviaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TriviaApplication.class, args);
 	}
-
 }
+
