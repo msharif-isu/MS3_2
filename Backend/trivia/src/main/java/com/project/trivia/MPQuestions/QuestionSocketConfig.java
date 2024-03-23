9a8fa143c3960cf8724327e1aceb2ca865a0f93e
rbruce01@iastate.edu <glpat-EKjJtPzKhL46BZ7nT7dw>
2024-03-23 11:22:44 -0500

    added randomizer and pulling question into websocket

---
+++ Backend/trivia/src/main/java/com/project/trivia/MPQuestions/QuestionSocketConfig.java
@@ -0,0 +1,14 @@
+package com.project.trivia.MPQuestions;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+public class QuestionSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
