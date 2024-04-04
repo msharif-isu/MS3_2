996e90c0151e74d0160647c19c538b3533789715
Alok Shrestha <aloks@iastate.edu>
2024-04-04 10:32:51 -0500

    Pulled in missing leaderboard websocket frontend files

--- Frontend/AndroidExample/app/src/main/java/url/RequestURLs.java
+++ Frontend/AndroidExample/app/src/main/java/url/RequestURLs.java
@@ -13,7 +13,6 @@ public class RequestURLs {
     public static final String SERVER_HTTP_USER_QUESTION_URL = "http://coms-309-034.class.las.iastate.edu:8080/question";
     public static final String SERVER_HTTP_QUESTION_URL = "http://10.0.2.2:8080/question";
     public static final String SERVER_HTTP_QUESTION_QUERY_URL = "http://10.0.2.2:8080/query";
-    public static final String SERVER_HTTP_SAVE_ANSWER_URL = "http://coms-309-034.class.las.iastate.edu:8080/answer";
     public static final String SERVER_HTTP_ANSWER_CHECKER_URL = "http://10.0.2.2:8080/question/answers";
     public static final String SERVER_HTTP_CREATE_USER_ANSWER_URL = "http://10.0.2.2:8080/answer/add";
     public static final String SERVER_HTTP_USER_ADD_POINTS_URL = "http://10.0.2.2:8080/users";
@@ -24,5 +23,5 @@ public class RequestURLs {
     public static final String SERVER_WEBSOCKET_URL = "ws://10.0.2.2:8080";
     public static final String SERVER_WEBSOCKET_URL_MULTIPLAYER = "ws://10.0.2.2:8080";
             //"ws://coms-309-034.class.las.iastate.edu:8080";
-    public static final String SERVER_WEBSOCKET_LEADERBOARD_URL = "ws://coms-309-034.class.las.iastate.edu:8080/leaderboard";
+    public static final String SERVER_WEBSOCKET_LEADERBOARD_URL = "ws://10.0.2.2:8080/leaderboard";
 }
