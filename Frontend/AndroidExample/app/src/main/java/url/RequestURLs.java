927f2887083dfd5856afa00d872da421df80338b
osamman <[200~glpat-2STCituse5BUyinw33Zy~>
2024-04-08 15:28:42 -0500

    Made it easier to switch between localhost and server, done by uncommenting one line.

--- Frontend/AndroidExample/app/src/main/java/url/RequestURLs.java
+++ Frontend/AndroidExample/app/src/main/java/url/RequestURLs.java
@@ -8,22 +8,22 @@ public class RequestURLs {
      * The URL to be used to refer to any HTTP requests made to the VM server.
      * Change only this to switch to server from localhost.
      */
-    //private static final String SERVER_BASE_URL = "://coms-309-034.class.las.iastate.edu:8080";
-    private static final String SERVER_BASE_URL = "://10.0.2.2:8080";
+    private static final String SERVER_BASE_URL = "://coms-309-034.class.las.iastate.edu:8080";
+//    private static final String SERVER_BASE_URL = "://10.0.2.2:8080";
     public static final String SERVER_HTTP_URL = "http" + SERVER_BASE_URL;
-    public static final String SERVER_HTTP_USER_QUESTION_URL = "http://coms-309-034.class.las.iastate.edu:8080/question";
-    public static final String SERVER_HTTP_QUESTION_URL = "http://10.0.2.2:8080/question";
-    public static final String SERVER_HTTP_QUESTION_QUERY_URL = "http://10.0.2.2:8080/query";
-    public static final String SERVER_HTTP_ANSWER_CHECKER_URL = "http://10.0.2.2:8080/question/answers";
-    public static final String SERVER_HTTP_CREATE_USER_ANSWER_URL = "http://10.0.2.2:8080/answer/add";
-    public static final String SERVER_HTTP_USER_ADD_POINTS_URL = "http://10.0.2.2:8080/users";
+    public static final String SERVER_HTTP_USER_QUESTION_URL = SERVER_HTTP_URL + "/question";
+    public static final String SERVER_HTTP_QUESTION_URL = SERVER_HTTP_URL + "/question";
+    public static final String SERVER_HTTP_QUESTION_QUERY_URL = SERVER_HTTP_URL + "/query";
+    public static final String SERVER_HTTP_ANSWER_CHECKER_URL = SERVER_HTTP_URL + "/question/answers";
+    public static final String SERVER_HTTP_CREATE_USER_ANSWER_URL = SERVER_HTTP_URL + "/answer/add";
+    public static final String SERVER_HTTP_USER_ADD_POINTS_URL = SERVER_HTTP_URL + "/users";
 
     /**
      * The URL to be used to refer to any WebSocket requests made to the VM server
      */
-    public static final String SERVER_WEBSOCKET_URL = "ws://10.0.2.2:8080";
+    public static final String SERVER_WEBSOCKET_URL = "ws" + SERVER_BASE_URL;
     public static final String SERVER_WEBSOCKET_URL_MULTIPLAYER = "ws" + SERVER_BASE_URL;
     //"ws://10.0.2.2:8080";
     //"ws://coms-309-034.class.las.iastate.edu:8080";
-    public static final String SERVER_WEBSOCKET_LEADERBOARD_URL = "ws://10.0.2.2:8080/leaderboard";
+    public static final String SERVER_WEBSOCKET_LEADERBOARD_URL =  SERVER_WEBSOCKET_URL + "/leaderboard";
 }
