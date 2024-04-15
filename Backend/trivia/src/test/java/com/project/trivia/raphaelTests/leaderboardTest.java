32ec9dd8c5c64cfadc140636cde087b83fa066a1
rbruce01@iastate.edu <glpat-EKjJtPzKhL46BZ7nT7dw>
2024-04-15 15:36:13 -0500

    fixed the testing error proceed to do test cases

--- Backend/trivia/src/test/java/com/project/trivia/raphaelTests/leaderboardTest.java
+++ Backend/trivia/src/test/java/com/project/trivia/raphaelTests/leaderboardTest.java
@@ -1,14 +1,16 @@
 package com.project.trivia.raphaelTests;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.project.trivia.Leaderboard.*;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-@SpringBootTest
-class leadboardTest {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class leaderboardTest {
 
 	@Autowired
 	private LeaderboardController controller;
