f9ec78a21b92e834ff28b7663e8289cea453e907
osamman <[200~glpat-2STCituse5BUyinw33Zy~>
2024-04-16 00:17:51 -0500

    Added buttons on bottom of screen for navigation. Currently not functional.

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,6 +14,8 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.example.androidexample.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button strBtn, jsonObjBtn, jsonArrBtn, imgBtn, questionBtn, queryButton, jeopardyButton;
@@ -20,15 +25,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String username;
     private int userId;
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SharedPreferences prefs = getSharedPreferences("UserData", MODE_PRIVATE);
-        username = prefs.getString("USERNAME","");
+        username = prefs.getString("USERNAME", "");
         userId = prefs.getInt("USER_ID", 0);
         Log.d("MainActivity", "Username from SharedPreferences: " + userId + username);
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new PlayFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.play) {
+                replaceFragment(new PlayFragment());
+            } else if (itemId == R.id.edit) {
+                replaceFragment(new EditFragment());
+            } else if (itemId == R.id.leaderboard) {
+                replaceFragment(new LeaderboardFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
 
         strBtn = findViewById(R.id.btnStringRequest);
         jsonObjBtn = findViewById(R.id.btnJsonObjRequest);
@@ -84,5 +107,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else if (id == R.id.btnJeopardy) {
             startActivity(new Intent(MainActivity.this, JeopardyActivity.class));
         }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
