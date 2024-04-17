a5ba65cf87b5f848682a8c99571c3b551b2c235d
rbruce01@iastate.edu <glpat-EKjJtPzKhL46BZ7nT7dw>
2024-04-16 23:30:23 -0500

    working on tests

--- Backend/trivia/src/test/java/com/project/trivia/raphaelTests/leaderboardTest.java
+++ Backend/trivia/src/test/java/com/project/trivia/raphaelTests/leaderboardTest.java
@@ -5,19 +5,38 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.project.trivia.Leaderboard.*;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class leaderboardTest {
 
+	@MockBean
+	LeaderboardRepository lbRepo;
+
 	@Autowired
 	private LeaderboardController controller;
 
-	@Test
-	void contextLoads() throws Exception{
-		assertThat(controller).isNotNull();
-	}
+	//@Autowired
+	//private MockMvc mockMvc;
 
+	/*
+	@Test
+	void getFirstLeaderboard() throws Exception {
+		this.mockMvc.perform(get("/leaderboard/1")).andDo(print()).andExpect(status().isOk()).andExpect(content().llbRepo.findById(1))
+	}
+	 */
+	/*
+	@Test
+	void getFirstLeaderboard() {
+		Leaderboard lb1 = lbRepo.findById(1);
+	}
+*/
 }
