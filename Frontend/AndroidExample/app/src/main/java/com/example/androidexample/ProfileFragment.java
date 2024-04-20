9461a1891dc90749157c594b0e47d56a90a3e236
osamman <[200~glpat-2STCituse5BUyinw33Zy~>
2024-04-20 16:19:31 -0500

    Updated to profile picture to use glide.

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/ProfileFragment.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.style.TtsSpan;
@@ -22,6 +23,8 @@ import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -38,6 +41,11 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -464,27 +472,27 @@ public class ProfileFragment extends Fragment {
     //backendUrl + "users/" + userId + "/profilePicture"
     private void getProfilePic() {
         // Make a request to fetch the profile picture
-        ImageRequest request = new ImageRequest(
-                backendUrl + "images/" + username,
-                new Response.Listener<Bitmap>() {
+        String imageUrl = backendUrl + "images/" + username;
+        RequestOptions requestOptions = new RequestOptions()
+                .transform(new RoundedCorners(100)) // Adjust the corner radius as needed
+                .placeholder(R.drawable.profile); // Placeholder image while loading
+        Glide.with(requireContext())
+                .asBitmap()
+                .load(imageUrl)
+                .apply(requestOptions)
+                .into(new SimpleTarget<Bitmap>() {
                     @Override
-                    public void onResponse(Bitmap response) {
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
                         // Set the retrieved profile picture to the ImageView
-                        imgView.setImageBitmap(response);
+                        imgView.setImageBitmap(resource);
                     }
-                },
-                0,
-                0,
-                ImageView.ScaleType.CENTER_INSIDE,
-                null,
-                new Response.ErrorListener() {
                     @Override
-                    public void onErrorResponse(VolleyError error) {
-                        //imgView.setImageResource(R.drawable.default_profile_image);
+                    public void onLoadFailed(@Nullable Drawable errorDrawable) {
+                        // Handle the case where image loading fails
+                        imgView.setImageResource(R.drawable.profile);
                     }
                 });
-        // Add the request to the RequestQueue
-        Volley.newRequestQueue(requireContext()).add(request);
     }
+
 
 }
