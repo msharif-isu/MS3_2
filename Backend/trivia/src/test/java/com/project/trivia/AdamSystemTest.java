491d4f6948cbb268fbbcb72213048e2c186eb551
adamal22@iastate.edu <adamal22@iastate.edu>
2024-04-20 23:02:10 -0500

    Simple starting test to understand how testing works

--- Backend/trivia/src/test/java/com/project/trivia/AdamSystemTest.java
+++ Backend/trivia/src/test/java/com/project/trivia/AdamSystemTest.java
@@ -1,9 +1,45 @@
 package com.project.trivia;
 
+import com.project.trivia.User.User;
+import com.project.trivia.User.UserController;
+import com.project.trivia.User.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
+import java.util.ArrayList;
+import java.util.List;
 
-@RunWith(SpringRunner.class)
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
 public class AdamSystemTest {
+
+    @Mock
+    private UserRepository userRepo;
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getUserByIdTest() throws Exception {
+        List<User> userList = new ArrayList<>();
+        when(userRepo.findAll()).thenReturn(userList);
+        mockMvc.perform(get("/users"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+
 }
+
