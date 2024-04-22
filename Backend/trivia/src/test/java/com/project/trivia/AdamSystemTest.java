993a517be4137d457cf5ad2297faf4e8776031b0
adamal22@iastate.edu <adamal22@iastate.edu>
2024-04-21 20:05:08 -0500

    First Test

--- Backend/trivia/src/test/java/com/project/trivia/AdamSystemTest.java
+++ Backend/trivia/src/test/java/com/project/trivia/AdamSystemTest.java
@@ -1,24 +1,21 @@
 package com.project.trivia;
 
 import com.project.trivia.User.User;
-import com.project.trivia.User.UserController;
 import com.project.trivia.User.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -34,12 +31,15 @@ public class AdamSystemTest {
 
     @Test
     public void getUserByIdTest() throws Exception {
-        List<User> userList = new ArrayList<>();
-        when(userRepo.findAll()).thenReturn(userList);
-        mockMvc.perform(get("/users"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-    }
+        User user = new User("Testalok", "password123", "aloks@iastate.edu");
+        userRepo.save(user);
 
+        when(userRepo.findById(user.getId())).thenReturn(user);
+
+        mockMvc.perform(get("/users/{id}", user.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(0));
+    }
 
 }
 
