93c938dcad800d1bb8ca4fef49f9d97b2d3e26ba
Alok Shrestha <aloks@iastate.edu>
2024-04-23 16:44:28 -0500

    Added testing tutorial and made system test class

---
+++ Frontend/AndroidExample/app/src/androidTest/java/com/example/androidexample/SystemTest1.java
@@ -0,0 +1,35 @@
+package com.example.androidexample;
+
+import android.support.test.rule.ActivityTestRule;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.StringEndsWith.endsWith;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+@LargeTest
+public class SystemTest1 {
+    private static final int SIMULATED_DELAY_MS = 500;
+
+    @Rule   // needed to launch the activity
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void answerQuestion() {
+        onView(withId(R.id.bottomNavigationView))
+                .perform(click());
+
+    }
+}
