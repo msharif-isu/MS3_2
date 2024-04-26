7c06f6ebba60e8def152f423f796b17e600021ca
Alok <aloks@iastate.edu>
2024-04-26 13:15:51 -0500

    Created instrumentation tests for JeopardyActivity

--- Frontend/AndroidExample/app/src/androidTest/java/com/example/androidexample/SystemTest1.java
+++ Frontend/AndroidExample/app/src/androidTest/java/com/example/androidexample/SystemTest1.java
@@ -6,65 +6,170 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isNotEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.fail;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.test.espresso.action.ViewActions;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+
+import url.RequestURLs;
+
 @RunWith(AndroidJUnit4.class)
 public class SystemTest1 {
 
     @Rule
-    public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<JeopardyActivity> rule = new ActivityScenarioRule<>(JeopardyActivity.class);
     private static final String VALID_USERNAME = "aloks";
     private static final String VALID_PASSWORD = "password123";
     private static final int VALID_USER_ID = 1;
+    private ArrayList<Question> questions = new ArrayList<>();
+
+    private void getQuestions(Context context) {
+        JsonArrayRequest questionRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                String.format("%s/question", RequestURLs.SERVER_HTTP_URL),
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonArray) {
+                        questions.clear();
+
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            try {
+                                Question q = new Question(jsonArray.getJSONObject(i));
+                                questions.add(q);
+                            } catch (JSONException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        try {
+                            throw volleyError;
+                        } catch (VolleyError e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        );
+
+        VolleySingleton.getInstance(context).addToRequestQueue(questionRequest);
+    }
 
     @Before
-    public void login() {
+    public void setup() {
         rule.getScenario().onActivity(activity -> {
-            SharedPreferences prefs = activity.getSharedPreferences("UserData", Context.MODE_PRIVATE);
-            if (prefs.getString("USERNAME", "").isEmpty()) {
-                onView(withId(R.id.login_username_edt)).perform(typeText(VALID_USERNAME), closeSoftKeyboard());
-                onView(withId(R.id.login_password_edt)).perform(typeText(VALID_PASSWORD), closeSoftKeyboard());
-                onView(withId(R.id.login_btn)).perform(click());
-                try {
-                    Thread.sleep(50000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+//            SharedPreferences prefs = activity.getSharedPreferences("UserData", Context.MODE_PRIVATE);
+//            if (prefs.getString("USERNAME", "").isEmpty()) {
+//                onView(withId(R.id.login_username_edt)).perform(typeText(VALID_USERNAME), closeSoftKeyboard());
+//                onView(withId(R.id.login_password_edt)).perform(typeText(VALID_PASSWORD), closeSoftKeyboard());
+//                onView(withId(R.id.login_btn)).perform(click());
+//                try {
+//                    Thread.sleep(50000);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+
+            getQuestions(activity.getApplicationContext());
         });
     }
-    @Test
-    public void checkLeaderboard() {
-        onView(withId(R.id.leaderboard)).perform(click());
+
+    private void openQuestion() {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        onView(withId(R.id.leaderboard_banner)).check(matches(isDisplayed()));
-    }
-    @Test
-    public void jeopardyTest() {
-        onView(withId(R.id.jeopardyButton)).perform(click());
+        // Click a 100 point question
+        onView(withId(R.id.jeopardy_button_00)).perform(click());
         try {
-            Thread.sleep(5000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        onView(withId(R.id.jeopardy_button_00)).perform(click());
-        onView(withId(R.layout.jeopardy_question_dialog)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testJeopardyBlankAnswer() {
+        openQuestion();
+
+        onView(withId(R.id.jeopardy_dialog_submit_button)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // If the dialog is still there, the answer wasn't submitted
+        onView(withId(R.id.jeopardy_dialog_submit_button)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testJeopardyCorrectAnswer() {
+        openQuestion();
+
+        String correctAnswer = "Philosophocles";
+
+        // Type in correct answer and submit
+        onView(withId(R.id.jeopardy_dialog_answer)).perform(typeText(correctAnswer), closeSoftKeyboard());
+        onView(withId(R.id.jeopardy_dialog_submit_button)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Check that question can't be selected again and that player got points
+        onView(withId(R.id.jeopardy_button_00)).check(matches(isNotEnabled()));
+        onView(withId(R.id.jeopardy_point_total_text)).check(matches(withText("Total Points: 100")));
+    }
+
+    @Test
+    public void testJeopardyIncorrectAnswer() {
+        openQuestion();
+
+        // Type in incorrect answer and submit
+        onView(withId(R.id.jeopardy_dialog_answer)).perform(typeText("Clearly wrong answer"), closeSoftKeyboard());
+        onView(withId(R.id.jeopardy_dialog_submit_button)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Check that question can't be selected again and that player didn't get points
+        onView(withId(R.id.jeopardy_button_00)).check(matches(isNotEnabled()));
+        onView(withId(R.id.jeopardy_point_total_text)).check(matches(withText("Total Points: 0")));
     }
 }
 
