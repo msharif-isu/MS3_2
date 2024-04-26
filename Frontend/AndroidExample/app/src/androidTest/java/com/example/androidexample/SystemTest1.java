ee8ac156404f4d44d8a14812b92d0d65cc3f7109
Alok Shrestha <aloks@iastate.edu>
2024-04-26 11:02:23 -0500

    Started adding system tests

--- Frontend/AndroidExample/app/src/androidTest/java/com/example/androidexample/SystemTest1.java
+++ Frontend/AndroidExample/app/src/androidTest/java/com/example/androidexample/SystemTest1.java
@@ -1,35 +1,70 @@
 package com.example.androidexample;
 
-import android.support.test.rule.ActivityTestRule;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-import androidx.test.filters.LargeTest;
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import android.content.Context;
+import android.content.SharedPreferences;
 
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.StringEndsWith.endsWith;
-
-@RunWith(AndroidJUnit4ClassRunner.class)
-@LargeTest
+@RunWith(AndroidJUnit4.class)
 public class SystemTest1 {
-    private static final int SIMULATED_DELAY_MS = 500;
 
-    @Rule   // needed to launch the activity
-    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+    @Rule
+    public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
+    private static final String VALID_USERNAME = "aloks";
+    private static final String VALID_PASSWORD = "password123";
+    private static final int VALID_USER_ID = 1;
 
+    @Before
+    public void login() {
+        rule.getScenario().onActivity(activity -> {
+            SharedPreferences prefs = activity.getSharedPreferences("UserData", Context.MODE_PRIVATE);
+            if (prefs.getString("USERNAME", "").isEmpty()) {
+                onView(withId(R.id.login_username_edt)).perform(typeText(VALID_USERNAME), closeSoftKeyboard());
+                onView(withId(R.id.login_password_edt)).perform(typeText(VALID_PASSWORD), closeSoftKeyboard());
+                onView(withId(R.id.login_btn)).perform(click());
+                try {
+                    Thread.sleep(50000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
     @Test
-    public void answerQuestion() {
-        onView(withId(R.id.bottomNavigationView))
-                .perform(click());
-
+    public void checkLeaderboard() {
+        onView(withId(R.id.leaderboard)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        onView(withId(R.id.leaderboard_banner)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void jeopardyTest() {
+        onView(withId(R.id.jeopardyButton)).perform(click());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        onView(withId(R.id.jeopardy_button_00)).perform(click());
+        onView(withId(R.layout.jeopardy_question_dialog)).check(matches(isDisplayed()));
     }
 }
+
